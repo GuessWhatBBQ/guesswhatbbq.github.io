e98082e96571fdbfd507758de54d86eb0b8b5f9d
GuessWhatBBQ <saminslm@gmail.com>
2024-05-18 04:05:32 +0600

    changes to switch to manual github actions

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import commonjs from "vite-plugin-commonjs";
 export default defineConfig({
   root: "src",
   build: {
-    outDir: "../src/dist",
+    outDir: "../dist",
   },
   plugins: [
     inject({
